cb951c3b7f4aa4674504d824ed0b1e9de7b608cc
sureshmami <41246534+sureshmami@users.noreply.github.com>
2020-12-09 19:10:47 +0530

    Create remote.py

---
+++ remote.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import paramiko
+
+
+key = paramiko.RSAKey.from_private_key_file(r'koushi.pem')
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ 
+out_file = open('connection_results.txt','w+')
+in_file = open(r'servers.txt') 
+for host in in_file: 
+      host=host.strip()
+      print ("Checking server",host)
+      
+      try:
+            ssh.connect(host, username="ubuntu", pkey = key)
+     
+            command = 'uptime'
+            (stdin, stdout, stderr) = ssh.exec_command(command)
+            for line in stdout.readlines():
+                  print ("Connected to" +" " + host + "and uptime is" + line)
+            ssh.close()
+ 
+      except Exception as e:
+            print(e)
+            
+ 
+in_file.close()
+out_file.close()
