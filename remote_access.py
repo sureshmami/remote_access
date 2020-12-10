185c73da6e7899810a53f0dfe662df49a383c42d
sureshmami <41246534+sureshmami@users.noreply.github.com>
2020-12-10 18:27:45 +0530

    Update and rename remote.py to remote_access.py

--- remote_access.py
+++ remote_access.py
@@ -3,28 +3,28 @@ import time
 import paramiko
 
 
-key = paramiko.RSAKey.from_private_key_file(r'koushi.pem')
+key = paramiko.RSAKey.from_private_key_file(r'/opt/python_scripts/suresh')
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
- 
-out_file = open('connection_results.txt','w+')
-in_file = open(r'servers.txt') 
-for host in in_file: 
+
+out_file = open(r'/opt/python_scripts/connection_results.txt','w+')
+in_file = open(r'/opt/python_scripts/servers.txt')
+for host in in_file:
       host=host.strip()
       print ("Checking server",host)
-      
+
       try:
             ssh.connect(host, username="ubuntu", pkey = key)
-     
+
             command = 'uptime'
             (stdin, stdout, stderr) = ssh.exec_command(command)
             for line in stdout.readlines():
                   print ("Connected to" +" " + host + "and uptime is" + line)
             ssh.close()
- 
+
       except Exception as e:
             print(e)
-            
- 
+
+
 in_file.close()
 out_file.close()
